32c8595cd3d8efa15d2a2e72bd31bccd8a7e73c9
Peter Williams <peter@newton.cx>
2018-03-23 16:35:28 -0400

    examples/account.rs: report in-progress wait times

--- examples/account.rs
+++ examples/account.rs
@@ -4,10 +4,12 @@
 /*! Demonstration of querying the Slurmdb job accounting database.
  */
 
+extern crate chrono;
 #[macro_use] extern crate clap;
 extern crate failure;
 extern crate slurm;
 
+use chrono::Utc;
 use clap::{Arg, App};
 use failure::Error;
 use std::process;
@@ -46,18 +48,23 @@ fn inner(jobid: &str) -> Result<i32, Error> {
 
     let db = slurm::DatabaseConnectionOwned::new()?;
     let jobs = db.get_jobs(&filter)?;
+    let now = Utc::now();
 
     for job in jobs.iter() {
         println!("{} {}", job.job_id(), job.job_name());
 
-        if let Some(d) = job.wait_duration() {
+        if let Some(d) = job.eligible_wait_duration() {
             println!("  time for job to become eligible to run: {} s", d.num_seconds());
         } else {
-            println!("  job not yet eligible to run");
+            let wait = now.signed_duration_since(job.submit_time());
+            println!("  job not yet eligible to run; time since submission: {} s", wait.num_seconds());
         }
 
         if let Some(d) = job.wait_duration() {
             println!("  wait time: {} s", d.num_seconds());
+        } else if let Some(t_el) = job.eligible_time() {
+            let wait = now.signed_duration_since(t_el);
+            println!("  job not yet started; time since eligibility: {} s", wait.num_seconds());
         } else {
             println!("  still waiting to start");
         }
@@ -65,8 +72,11 @@ fn inner(jobid: &str) -> Result<i32, Error> {
         if let Some(d) = job.wallclock_duration() {
             println!("  wallclock runtime: {} s", d.num_seconds());
             println!("  exit code: {}", job.exit_code().unwrap());
+        } else if let Some(t_st) = job.start_time() {
+            let wait = now.signed_duration_since(t_st);
+            println!("  job not yet finished; time since start: {} s", wait.num_seconds());
         } else {
-            println!("  job has not yet finished");
+            println!("  job not yet finished");
         }
     }
 
