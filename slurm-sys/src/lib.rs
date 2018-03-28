66e3616d1af305aea953c9a8400fa1098fe1f888
Peter Williams <peter@newton.cx>
2018-03-28 16:41:07 -0400

    Add some crate-level docs to slurm_sys.

--- slurm-sys/src/lib.rs
+++ slurm-sys/src/lib.rs
@@ -1,5 +1,16 @@
-// Copyright 2017 Peter Williams <peter@newton.cx> and collaborators
+// Copyright 2017-2018 Peter Williams <peter@newton.cx> and collaborators
 // Licensed under the MIT license.
+
+//! Low-level bindings to the `libslurm` and `libslurmdb` libraries.
+//!
+//! The [Slurm](https://slurm.schedmd.com/) workload manager a system for
+//! scheduling and running jobs on large computing clusters. It is often used
+//! in scientific HPC (high-performance computing) contexts.
+//!
+//! These bindings provide nothing beyond the barest minimum needed to
+//! interface to the C code unsafely. As such, this crate has no documentation
+//! beyond the text you see here. Use a higher-level Rust crate in application
+//! code.
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
